42ae4eef04518eaaa83c9f7dc795da3c79fcb247
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-12-04 14:07:01 +0530

    Errors Outside Express Unhandled Rejections

--- server.js
+++ server.js
@@ -25,8 +25,14 @@ mongoose
 // console.log(process.env);
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running ${port}...`);
 });
 
-// Test
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
