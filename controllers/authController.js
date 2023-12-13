bc518a9015ec9a8c13e03b5fb24d48e949435a3e
Kavishcan <108811385+Kavishcan@users.noreply.github.com>
2023-12-14 00:43:25 +0530

    Add JWT token verification and user validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -68,10 +69,25 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
 
   // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to this token does no longer exist.'),
+      401,
+    );
+  }
 
   // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401),
+    );
+  }
 
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
   next();
 });
